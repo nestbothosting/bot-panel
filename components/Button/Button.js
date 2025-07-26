5572503feebdb710702e893f4f1f4e1fa71ee8d4
abijith <abijithumax100@gmail.com>
2025-07-25 20:51:51 -0500

    fix button style issue

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -3,11 +3,20 @@
 import React from 'react'
 import style from './Button.module.css'
 
+const Change = async (e) => {
+  const buttonName = e.target.innerText;
+  e.target.innerText = "loading.."
+  setTimeout(() => {
+    e.target.innerText = buttonName
+  },6000)
+  
+}
+
 export default function Button({ name='click', color, gbcolor, border }) {
     if(!border){
         border = 'none'
     }
   return (
-    <button style={{ color:color, backgroundColor:gbcolor, border:border }} className={ style.btn }>{name}</button>
+    <button onClick={(e) => Change(e)} style={{ color:color, backgroundColor:gbcolor, border:border }} className={ style.btn }>{name}</button>
   )
 }
